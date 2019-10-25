74683da5de92e41c7ef2990b59934781213f1c06
alumno <alumno@LAB00PC00>
2019-10-24 22:11:25 -0300

    archivos

---
+++ clases/Clase 17/src/clase_Archivos.c
@@ -0,0 +1,44 @@
+/*
+ ============================================================================
+ Name        : clase_Archivos.c
+ Author      : Sergio
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*int main(void) {
+
+	FILE *fp=fopen("archivo.txt","w");
+	int i=4569;
+	if(fp!=NULL)
+	{
+		printf("\nabri el archivo\n");
+
+	fwrite(&i,sizeof(int),1,fp);
+	fclose(fp);
+	}
+	FILE *fp = fopen("archivo.txt", "r");
+	if (fp != NULL)
+	{
+		int i;
+		int j;
+		int k;
+		int l;
+		printf("\nabri el archivo\n");
+		int r=fscanf(fp,"%d,%d,%d,%d",&i,&j,&k,&l);
+		printf("\nfscanf devolvio %d\n",r);
+		fclose(fp);
+		printf("\nlei de la var %d\n", i);
+		printf("\nlei de la var %d\n",j);
+		printf("\nlei de la var %d\n",k);
+		printf("\nlei de la var %d\n",l);
+
+	}
+
+}
+*/
